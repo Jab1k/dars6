fa6cba0abb8fa0132d1eca9e30bda43d791e2104
Jab1k <jobir4166@gmail.com>
2022-11-15 18:17:49 +0500

    expanded misol

--- lib/dars.dart
+++ lib/dars.dart
@@ -15,51 +15,80 @@ class Dars extends StatelessWidget {
           backgroundColor: Colors.greenAccent,
           title: const Text('Geeksforgeeks'),
         ),
-        body: Row(
+        body: Column(
           children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                width: 250,
-                height: 100,
-                color: Colors.black12,
-                child: const Padding(
-                  padding: EdgeInsets.only(top: 30, left: 10),
-                  child: Text(
-                    'A',
-                    style: TextStyle(fontSize: 30),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: 250,
+                    height: 100,
+                    color: Colors.black12,
+                    child: const Padding(
+                      padding: EdgeInsets.only(top: 30, left: 10),
+                      child: Text(
+                        'A',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    width: 250,
+                    height: 100,
+                    color: Colors.yellow,
+                    child: const Padding(
+                      padding: EdgeInsets.only(top: 30, left: 10),
+                      child: Text(
+                        'B',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    width: 250,
+                    height: 100,
+                    color: Colors.red,
+                    child: const Padding(
+                      padding: EdgeInsets.only(top: 30, left: 10),
+                      child: Text(
+                        'C',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
             Expanded(
-              flex: 2,
-              child: Container(
-                width: 250,
-                height: 100,
-                color: Colors.yellow,
-                child: const Padding(
-                  padding: EdgeInsets.only(top: 30, left: 10),
-                  child: Text(
-                    'B',
-                    style: TextStyle(fontSize: 30),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 200,
+                      color: Colors.green,
+                    ),
                   ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 6,
-              child: Container(
-                width: 250,
-                height: 100,
-                color: Colors.red,
-                child: const Padding(
-                  padding: EdgeInsets.only(top: 30, left: 10),
-                  child: Text(
-                    'C',
-                    style: TextStyle(fontSize: 30),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.yellow,
+                    ),
                   ),
-                ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                  )
+                ],
               ),
             )
           ],
